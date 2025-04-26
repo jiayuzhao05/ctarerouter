37b5694587c1aadec0de4391d23c182fe74c9cac
Ethan Evans <ethanevans.j@gmail.com>
2025-04-26 17:56:22 -0500

    load csv

--- GUI/cleanstops.py
+++ GUI/cleanstops.py
@@ -4,13 +4,13 @@ from app import db, Station  # import your app's db and Station model
 import sqlite3
 
 # Load your CSV
-df = pd.read_csv('CTA_list_stops.csv')  # <-- change this to your CSV filename
+df = pd.read_csv('GUI/ctastops.csv')  # <-- change this to your CSV filename
 
 # subset only the stop name, line colors and location
 df = df[['STATION_NAME','Location']]
 
 # convert location to lat long
-df[['latitude', 'longitude']] = df['Location'].str.extract(r'\(([-\d.]+),\s*([-\d.]+)\)').astype(float)
+df[['lat', 'lng']] = df['Location'].str.extract(r'\(([-\d.]+),\s*([-\d.]+)\)').astype(float)
 df.drop(columns='Location', inplace=True)
 
 # Example of your columns: STOP_ID, Location
@@ -22,6 +22,6 @@ df['STATION_NAME'] = df.index
 df.rename(columns={'STATION_NAME':'station_id'}, inplace=True)
 
 # Connect to sqlite3 and upload dataframe to sqlite table
-conn = sqlite3.connect('locations.db')
-df.to_sql('locations',conn,if_exists='replace',index=False)
+conn = sqlite3.connect('stations_routes.db')
+df.to_sql('stations',conn,if_exists='replace',index=False)
 conn.close()
